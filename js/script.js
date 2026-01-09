c255e0d4e23682a77089122fd90865f04d17a99c
mklfactory <mklfactory@gmail.com>
2026-01-09 12:26:54 +0100

    update api url

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // =========================
 // CONFIGURATION API
 // =========================
-const API_URL = "http://127.0.0.1:8000//api/v1/titles/";
+const API_URL = "http://127.0.0.1:8000/api/v1/titles/";
 const CATEGORY_URL = "http://127.0.0.1:8000/api/v1/genres/";
 
 
